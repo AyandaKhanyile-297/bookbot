d86d445724a2fc74d9bd737557ac22f3daa91c3a
AyandaKhanyile-297 <Khanyile_Ayanda@proton.me>
2026-01-01 17:11:41 +0200

    D: added functions to stats.py and main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, character_count
 
 def get_book_text(filepath):
     file_contents = ""
@@ -7,11 +7,17 @@ def get_book_text(filepath):
         file_contents = file.read()
         
     return file_contents
+
+def display_info(story):
+    print(story)
+
     
 def main():
     book_path = "books/frankenstein.txt"
     story = get_book_text(book_path)
     num_words = word_count(story)
     print(f"Found {num_words} total words")
+    num_characters = character_count(story)
+    display_info(num_characters)
     
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def word_count(paragraph):
     words = paragraph.split()
     return len(words)
+
+def character_count(paragraph):
+    dictionary = {}
+    book = paragraph.split()
+    
+    for word in book:
+        word = word.lower()
+        
+        for character in word:
+            if character in dictionary:
+                dictionary[character] += 1
+            else:
+                dictionary[character] = 1
+
+    return dictionary
+    
